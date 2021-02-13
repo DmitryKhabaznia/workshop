9f1d3ba061db3711d14bf0226c79531f91740c94
Dmytro Khabaznia <Dmytro_Khabaznia@epam.com>
2021-02-13 16:34:42 +0200

    some configs for db

--- src/main/java/workshop/db/ConnectionManager.java
+++ src/main/java/workshop/db/ConnectionManager.java
@@ -21,47 +21,47 @@ public class ConnectionManager {
     private static final Logger LOG = Logger.getLogger(ConnectionManager.class);
     private static final String CONNECTION_WAS_NOT_INSTALLED_ERROR_MESSAGE = "Connection was not installed.";
     private static final String CANNOT_GET_CONNECTION_ERROR_MESSAGE = "Can't get connection.";
-    private static final String DATA_SOURCE_NAME = "java:/comp/env/jdbc/shop";
+    private static final String DATA_SOURCE_NAME = "jdbc:mysql://localhost:3306/shop";
     private DataSource dataSource;
 
     public ConnectionManager() throws DBException {
-//        try {
-//            Context initContext = new InitialContext();
-//            dataSource = (DataSource) initContext.lookup(DATA_SOURCE_NAME);
-//            LOG.debug("Data source ==> " + dataSource);
-//        } catch (NamingException ex) {
-//            LOG.error(CONNECTION_WAS_NOT_INSTALLED_ERROR_MESSAGE, ex);
-//            throw new DBException(CONNECTION_WAS_NOT_INSTALLED_ERROR_MESSAGE, ex);
-//        }
+        try {
+            Context initContext = new InitialContext();
+            dataSource = (DataSource) initContext.lookup(DATA_SOURCE_NAME);
+            LOG.debug("Data source ==> " + dataSource);
+        } catch (NamingException ex) {
+            LOG.error(CONNECTION_WAS_NOT_INSTALLED_ERROR_MESSAGE, ex);
+            throw new DBException(CONNECTION_WAS_NOT_INSTALLED_ERROR_MESSAGE, ex);
+        }
     }
 
-    public Connection getConnection() {
-        try {
-            URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-            return DriverManager.getConnection(dbUrl, username, password);
-        } catch (URISyntaxException | SQLException ex) {
-            LOG.error(CANNOT_GET_CONNECTION_ERROR_MESSAGE);
-        }
-        return null;
-    }
+//    public Connection getConnection() {
+//        try {
+//            URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+//            String username = dbUri.getUserInfo().split(":")[0];
+//            String password = dbUri.getUserInfo().split(":")[1];
+//            String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+//            return DriverManager.getConnection(dbUrl, username, password);
+//        } catch (URISyntaxException | SQLException ex) {
+//            LOG.error(CANNOT_GET_CONNECTION_ERROR_MESSAGE);
+//        }
+//        return null;
+//    }
 
     /**
      * Returns {@link Connection} from {@link DataSource}.
      *
      * @return {@link Connection} from {@link DataSource}
      */
-//    public Connection getConnection() {
-//        Connection connection = null;
-//        try {
-//            connection = dataSource.getConnection();
-//        } catch (SQLException ex) {
-//            LOG.error(CANNOT_GET_CONNECTION_ERROR_MESSAGE, ex);
-//        }
-//        return connection;
-//    }
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException ex) {
+            LOG.error(CANNOT_GET_CONNECTION_ERROR_MESSAGE, ex);
+        }
+        return connection;
+    }
 
 }
 
